648d93dad1ff2d3e0e1a9c8bcac5f5fef6be86bc
Owen Pan <owenpiano@gmail.com>
2024-06-22 19:38:31 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,9 +10,10 @@ for d in $depends; do
 	type -f $d &> /dev/null || { echo 'Please rerun this script after restarting Haiku.'; exit; }
 done
 
+project=llvm-project
 version=18.1.8
 assets='clang cmake llvm third-party'
-prefix=https://github.com/llvm/llvm-project/releases/download/llvmorg-$version
+prefix=https://github.com/llvm/$project/releases/download/llvmorg-$version
 suffix=$version.src.tar.xz
 
 for a in $assets; do
@@ -20,23 +21,25 @@ for a in $assets; do
 	test -e $tarball || wget -N $prefix/$tarball
 done
 
-mkdir -pv llvm-project
-cd llvm-project
+if [ -e $project ]; then
+	echo "Please rerun this script after removing $project"
+	exit
+fi
 
-extract()
-{
-	mkdir -pv $1
-	echo Extracting $1 ...
-	tar xf ../$(basename $1)-$suffix -C $1 --strip-components=1 --skip-old-files
-}
+mkdir -v $project
+cd $project
 
 for a in $assets; do
-	test -e $a || extract $a
+	mkdir -v $a
+	echo -n "Extracting $a"
+	tar xf ../$(basename $a)-$suffix -C $a --strip-components=1 --checkpoint=.1000
+	echo
 done
+
+patch -N -p1 -r - < ../v$version.diff || :
 
 dir=build
 cmake -Wno-dev -S llvm -B $dir -G Ninja -DLLVM_ENABLE_PROJECTS=clang -DCMAKE_BUILD_TYPE=Release \
 	-DCMAKE_SKIP_RPATH=On
-patch -N -p1 -r - < ../v$version.diff || :
 ninja -C $dir clang-format
 strip -sv $dir/bin/clang-format
